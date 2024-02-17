230e038938a441713e02eeb104ac968aed033a58
Vladyslav Masokha <vladyslavmasokha2006@gmail.com>
2024-02-17 22:21:13 +0200

    added header in userProfile

--- src/assets/screens/userProfilePage/UserProfilePage.tsx
+++ src/assets/screens/userProfilePage/UserProfilePage.tsx
@@ -1,8 +1,17 @@
+import { Header } from '../../ui/Header/Header'
+import styles from './UserProfile.module.scss'
+
 const UserProfilePage = () => {
 	return (
-		<div className='wrapper'>
-			<h2>В розробці!</h2>
-		</div>
+		<>
+			<Header />
+
+			<div className={styles.about}>
+				<div className='wrapper'>
+					<h2>В розробці!</h2>
+				</div>
+			</div>
+		</>
 	)
 }
 
