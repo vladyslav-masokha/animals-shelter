124cf1275934aac454f9174f832acfcca11f1bb8
Vladyslav Masokha <vladyslavmasokha2006@gmail.com>
2024-02-17 19:16:06 +0200

    added styles

--- src/assets/components/AnimalCards/AnimalCard.tsx
+++ src/assets/components/AnimalCards/AnimalCard.tsx
@@ -24,7 +24,7 @@ const AnimalCard: React.FC<AnimalCardProps> = ({ animals }) => {
 					</Link>
 				))
 			) : (
-				<p>Animals is not defined!</p>
+				<p className={styles.not_animals}>Тваринки не знайдені!</p>
 			)}
 		</>
 	)
